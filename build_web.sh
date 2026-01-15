3090a6f6827694ebf2710220cfa312b962e3201b
eastcoastcoder <ethanx94@gmail.com>
2026-01-15 00:25:04 -0500

    testing...

--- build_web.sh
+++ build_web.sh
@@ -12,9 +12,11 @@ fi
 
 # Build the web version
 echo "Running pygbag build..."
-pygbag --build .
+pygbag --build --cdn https://pygame-web.github.io/archives/0.9/ .
 
 echo ""
 echo "Build complete!"
 echo "To test locally, run: python -m http.server 8000 --directory build/web"
 echo "Then open: http://localhost:8000"
+echo ""
+echo "NOTE: If you see 404 errors for pygame packages, packages will be loaded from CDN."
